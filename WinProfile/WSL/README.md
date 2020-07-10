acf11e6941efa0d757c2aab4522b1cba4cf8e920
Diffumist <misanzhiwu@gmail.com>
2020-07-10 22:50:17 +0800

    feat: network.sh

--- WinProfile/WSL/README.md
+++ WinProfile/WSL/README.md
@@ -1,6 +1,5 @@
 # 注意事项
 
-- `network1.sh` 用于解决 WSL1 下网络代理初始化问题，依端口实际情况修改
-- `network2.sh` 同上，WSL2 版本
+- `network.sh` 用于解决 WSL 下网络代理初始化问题
 - `initzsh.sh` zsh 初始化，确保已成功配置网络和安装 zsh
 - `.zshrc` 一个 zsh 参考配置
---
+++ WinProfile/WSL/network.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+gitconfig=".gitconfig"
+curlrc=".curlrc"
+if [ ! -f "$gitconfig" ]; then
+    touch "$gitconfig"
+fi
+if [ ! -f "$curlrc" ]; then
+    touch "$curlrc"
+fi
+echo "输入 WSL 版本"
+read ver
+clear
+echo "输入网络端口"
+read port
+clear
+if [ $ver == 1 ]
+then
+    sed -i '/\[http]/,$d' ~/.gitconfig
+    echo -e '[http]\nproxy=socks5://127.0.0.1:'${port}'\n[https]\nproxy=socks5://127.0.0.1:'${port}'' >> ~/.gitconfig
+    sed -i '/socks5.*/d' ~/.curlrc
+    echo 'socks5=127.0.0.1:'${port}'' >> ~/.curlrc
+    export ALL_PROXY=socks5://127.0.0.1:'${port}'
+    export all_proxy=socks5://127.0.0.1:'${port}'
+
+elif [ $ver == 2 ]
+then
+    export WIN_IP=`cat /etc/resolv.conf | grep nameserver | awk '{print $2}'`
+    sed -i '/\[http]/,$d' ~/.gitconfig
+    echo -e '[http]\nproxy=socks5://'${WIN_IP}':'${port}'\n[https]\nproxy=socks5://'${WIN_IP}':'${port}'' >> ~/.gitconfig
+    sed -i '/socks5.*/d' ~/.curlrc
+    echo 'socks5='${WIN_IP}':'${port}'' >> ~/.curlrc
+    export ALL_PROXY=socks5://'${WIN_IP}':'${port}'
+    export all_proxy=socks5://'${WIN_IP}':'${port}'
+fi
+
+
+echo 'WSL代理配置已初始化'
+curl https://ifconfig.me/
