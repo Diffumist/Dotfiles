aaffdcf96d27b4b432379006b0379dc06f55da25
Diffumist <misanzhiwu@gmail.com>
2020-07-10 23:10:20 +0800

    feat: network.sh

--- WinProfile/WSL/network.sh
+++ WinProfile/WSL/network.sh
@@ -2,29 +2,24 @@
 
 gitconfig=".gitconfig"
 curlrc=".curlrc"
+
 if [ ! -f "$gitconfig" ]; then
     touch "$gitconfig"
 fi
 if [ ! -f "$curlrc" ]; then
     touch "$curlrc"
 fi
-echo "输入 WSL 版本"
-read ver
-clear
-echo "输入网络端口"
-read port
-clear
-if [ $ver == 1 ]
-then
+
+wslnet1(){
     sed -i '/\[http]/,$d' ~/.gitconfig
     echo -e '[http]\nproxy=socks5://127.0.0.1:'${port}'\n[https]\nproxy=socks5://127.0.0.1:'${port}'' >> ~/.gitconfig
     sed -i '/socks5.*/d' ~/.curlrc
     echo 'socks5=127.0.0.1:'${port}'' >> ~/.curlrc
     export ALL_PROXY=socks5://127.0.0.1:'${port}'
     export all_proxy=socks5://127.0.0.1:'${port}'
+}
 
-elif [ $ver == 2 ]
-then
+wslnet2(){
     export WIN_IP=`cat /etc/resolv.conf | grep nameserver | awk '{print $2}'`
     sed -i '/\[http]/,$d' ~/.gitconfig
     echo -e '[http]\nproxy=socks5://'${WIN_IP}':'${port}'\n[https]\nproxy=socks5://'${WIN_IP}':'${port}'' >> ~/.gitconfig
@@ -32,6 +27,16 @@ then
     echo 'socks5='${WIN_IP}':'${port}'' >> ~/.curlrc
     export ALL_PROXY=socks5://'${WIN_IP}':'${port}'
     export all_proxy=socks5://'${WIN_IP}':'${port}'
+}
+
+echo "输入 WSL 版本"
+read ver
+echo "输入网络端口"
+read port
+if [ $ver == 1 ]; then
+welnet1
+elif [ $ver == 2 ]; then
+wslnet2
 fi
 
 
